361f2d1c1c3448eab21eeafa60a4e948a223beef
Chris <chris@ceebs.co.uk>
2020-02-14 20:46:43 +0000

    Modify Post to allow getting and deleting posts by slug

--- models/Post.php
+++ models/Post.php
@@ -1,8 +1,12 @@
 <?php
 require_once('Database.php');
+require_once('./classes/Util.php');
 
 class Post
 {
+    public $title;
+    public $body;
+    public $slug;
     private $pdo;
 
     // Grab an instance of the Database class and PDO object.
@@ -17,10 +21,10 @@ class Post
     {
         try
         {
-            $opertaion = $this->pdo->prepare("SELECT title, body FROM posts");
+            $opertaion = $this->pdo->prepare("SELECT title, body, slug FROM posts");
             $opertaion->execute();
 
-            $result = $opertaion->fetchAll(PDO::FETCH_OBJ);
+            $result = $opertaion->fetchAll(PDO::FETCH_CLASS, "Post");
 
             return $result;
         }
@@ -34,12 +38,15 @@ class Post
     // Create a post from the passed title and body parameters.
     public function create($title, $body)
     {
+        $slug = Util::slugify($title);
+
         try
         {
-            $opertaion = $this->pdo->prepare("INSERT INTO posts (title, body) VALUES(:title, :body)");
+            $opertaion = $this->pdo->prepare("INSERT INTO posts (title, body, slug) VALUES(:title, :body, :slug)");
             $success = $opertaion->execute([
                 ':title' => $title,
-                ':body' => $body
+                ':body' => $body,
+                ':slug' => $slug
             ]);
 
             if($success)
@@ -56,5 +63,58 @@ class Post
             http_response_code(500);
             die("Server Error: Creating - " . $e->getMessage());
         }
+    }
+
+    // Get post by slug.
+    public function getPostBySlug($slug)
+    {
+        try
+        {
+            $opertaion = $this->pdo->prepare("SELECT title, body, slug FROM posts WHERE slug=:slug LIMIT 1");
+            $opertaion->execute([
+                ':slug' => $slug
+            ]);
+
+            $result = $opertaion->fetchObject("Post");
+
+            return $result;
+        }
+        catch(PDOException $e)
+        {
+            http_response_code(500);
+            die("Server Error: Fetching - " . $e->getMessage());
+        }  
+    }
+
+    public function deleteBySlug($slug)
+    {
+        try
+        {
+            $opertaion = $this->pdo->prepare("SELECT * FROM posts WHERE slug=:slug LIMIT 1");
+            $opertaion->execute([
+                ':slug' => $slug
+            ]);
+
+            $result = $opertaion->fetchObject("Post");
+
+            if($result)
+            {
+                $opertaion = $this->pdo->prepare("DELETE FROM posts WHERE slug=:slug");
+                if($opertaion->execute([ ':slug' => $slug ]))
+                {
+                    return true;
+                }
+                else
+                {
+                    http_response_code(500);
+                    die("Server Error: Something went wrong during deletion.");
+                }
+            }
+        }
+        catch(PDOException $e)
+        {
+            http_response_code(500);
+            die("Server Error: Fetching - " . $e->getMessage());
+        } 
     }
 }
