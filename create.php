20bb1d77c1279649b72ff7e2ffe9b29ecee50eba
Chris <chris@ceebs.co.uk>
2020-02-15 09:25:01 +0000

    Modify both index and create views

--- create.php
+++ create.php
@@ -1,19 +1,46 @@
 <?php
+session_start();
 require_once('controllers/createController.php');
-
-$title = "Create";
 
 require_once('partials/_header.php');
 ?>
 
 <div class="hero-body">
     <div class="container">
-        <h1 class="title has-text-centered">
-            Create
-        </h1>
         <div class="columns">
             <div class="column"></div>
-            <div class="column">
+            <div class="column is-half">
+                <div class="level">
+                    <div class="level-left">
+                        <h1 class="title">
+                            <a href="index.php">Posts</a> > Create
+                        </h1>
+                    </div>
+                    <div class="level-right">
+                        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+                            <button class="button is-success is-outlined is-small" type="submit" name="sample_posts_submit">
+                                <span class="icon">
+                                    <i class="fas fa-check"></i>
+                                </span>
+                                <span>Create Sample Posts</span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+                <?php if(isset($_SESSION['message'])) { ?>
+                    <article class="message <?php echo $_SESSION['message_type']; ?>">
+                        <div class="message-body">
+                            <?php echo $_SESSION['message']; ?>
+                        </div>
+                    </article>
+                <?php
+                        unset($_SESSION['message_type']);
+                        unset($_SESSION['message']);
+                    } 
+                ?>
+                
+                <hr>
                 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
                     <div class="field">
                         <label class="label">Title</label>
@@ -30,7 +57,7 @@ require_once('partials/_header.php');
                     </div>
 
                     <div class="control">
-                        <button class="button is-link" name="create_submit" type="submit">Submit</button>
+                        <button class="button is-success is-outlined" name="create_submit" type="submit">Create Post</button>
                     </div>
                 </form>
             </div>
--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
+session_start();
 require_once('models/Post.php');
 
-$title = "Index";
-
 require_once('partials/_header.php');
-?>
-<div class="container">
-    <div class="columns">
-        <div class="column"></div>
-        <div class="column is-half">
-            <?php 
-                $post = new Post();
-                $results = $post->getPosts();
 
-                if(is_array($results))
-                {
-                    foreach ($results as $r) 
-                    {
-                        ?> <h2><?php echo $r->title ?></h2> <?php
-                        ?> <p><?php echo $r->body ?></p> <?php
-                    }
-                }
-            ?>
-        </div>
-        <div class="column"></div>
-    </div>
-</div>
-    
-<?php require_once('partials/_footer.php'); ?>
+// Require the readController to handle URL Params and render relevent partial.
+require_once('controllers/readController.php');
+
+require_once('partials/_footer.php');
