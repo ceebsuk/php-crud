d23d3a76281a2cb232d3af4caba7b8907cb0d1f6
Chris <chris@ceebs.co.uk>
2020-02-15 09:23:36 +0000

    Modify footer and header partials

--- partials/_header.php
+++ partials/_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?></title>
+    <title>Posts</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 </head>
 <body>
