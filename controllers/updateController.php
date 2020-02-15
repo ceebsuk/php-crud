44926b3e43795748b728a9ed4b98fcaa758c6282
Chris <chris@ceebs.co.uk>
2020-02-15 09:50:37 +0000

    Add check whether new information entered & handle bad request

--- controllers/updateController.php
+++ controllers/updateController.php
@@ -12,13 +12,14 @@ if(isset($_GET['post']))
 
     if(!$post)
     {
+        Util::flashMessage("danger", 'Failed: Post not found.');
         http_response_code(404);
         header('Location: index.php');
+        exit();
     }
 }
-
 // Check to see if the form has been submitted.
-if(isset($_POST['update_submit']))
+else if(isset($_POST['update_submit']))
 {
     $post = new Post();
     // Make sure the passed url is in the right slug format.
@@ -33,18 +34,37 @@ if(isset($_POST['update_submit']))
     // Check to see whether any inputs are empty.
     if(!empty($title) || !empty($body))
     {
-        // Update the post which returns a bool we can check for.
-        if($post->update($title, $body, $slug))
+        // Check to see if any of the post has changed.
+        if($post->title != $title || $post->body != $body)
         {
-            Util::flashMessage("success", 'Success: Post updated successfully. <a href="index.php">Back to Posts</a>');
+            // Update the post which returns a bool we can check for.
+            if($post->update($title, $body, $slug))
+            {
+                Util::flashMessage("success", 'Success: Post updated successfully. <a href="index.php">Back to Posts</a>');
+                header('Location: update.php?post=' . $slug);
+                exit();
+            }
+            else
+            {
+                Util::flashMessage("danger", "Failed: Couldn't update Post.");
+            }
         }
         else
         {
-            Util::flashMessage("danger", "Failed: Couldn't update Post.");
+            Util::flashMessage("danger", "Failed: The post data needs to be changed in order to update.");
+            header('Location: update.php?post=' . $slug);
+            exit();
         }
     }
     else
     {
         Util::flashMessage("danger", "Failed: The title of body fields can't be empty.");
     }
+}
+else
+{
+    Util::flashMessage("danger", 'Failed: Post not found.');
+    http_response_code(404);
+    header('Location: index.php');
+    exit();
 }
